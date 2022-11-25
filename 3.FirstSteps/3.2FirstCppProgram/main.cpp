dbcd0f05e8bfd77cf73d9b15d1b35f2c3f4e4a61
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-25 09:51:45 +0000

    first c++ program

--- 3.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 int main(){
-
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number2" << std::endl;
-    std::cout << "Number3" << std::endl;
+    std::cout << "Hello from Main!" << std::endl;
 
     return 0;
 }
+
+// the iostream package is a builtin package used to write to terminal
+// Main function is the entrypoint for c++ code, when you run the program this is what is run first
+// All main functions in c++ return something (usually 0) at end. If not explicitly then compiler adds it for you.  
+    // This is used as a signature for operating system to know your program finished successfully
